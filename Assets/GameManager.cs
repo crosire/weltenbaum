bcf3ec2fc278b6c3becb8a4362161e28d2b2fcf9
crosire <crosiredev@gmail.com>
2016-01-29 21:40:24 +0100

    Add basic game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public enum GameState
+{
+	Title,
+	Running,
+	Paused,
+	Won,
+	Lost,
+}
+
+public class GameManager : MonoBehaviour
+{
+	#region Inspector Variables
+	#endregion
+
+	GameState _gameState = GameState.Title;
+
+	private static GameManager Singleton { get; set; }
+
+	void Awake()
+	{
+		Debug.Assert(Singleton == null, "Cannot create multiple instances of the 'GameManager' singleton class.");
+
+		Singleton = this;
+	}
+}
