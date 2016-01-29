a152d32b20273acf2d851137697d836e7b88fa91
Elias Steurer <i@kelteseth.net>
2016-01-29 23:25:45 +0100

    Fix opacity values and useless Gamemode enum

--- Assets/UI/Scripts/Interface.cs
+++ Assets/UI/Scripts/Interface.cs
@@ -10,9 +10,6 @@ public class Interface : MonoBehaviour
 	public Button[] _buttons;
 	public CanvasGroup[] _pages;
 
-	bool[] _isActive = new bool[2];
-	
-
 	[Header("Interface")]
 	public CanvasGroup _interface;
 
@@ -41,7 +38,6 @@ public class Interface : MonoBehaviour
 		sequence
 			.Append(_menu.DOFade(0f, 1f))
 			.Append(_interface.DOFade(1f, .5f));
-		GameState.Running;
 	}
 
 	public void OnHelpPressed()
