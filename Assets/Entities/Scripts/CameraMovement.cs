fc555554c68f76e1efdb371af43fc8f106d26b3d
Elias Steurer <i@kelteseth.net>
2016-01-30 15:30:03 +0100

    Patrick did stuff

--- Assets/Entities/Scripts/CameraMovement.cs
+++ Assets/Entities/Scripts/CameraMovement.cs
@@ -5,40 +5,54 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour
 {
 	public Transform _cameraFocus;
-	public Transform[] _lanePositions;
+	public Transform[] _laneCameraPositions;
+	public Transform[] _cameraTransitionsPositions;
+	int _preLaneIndex;
 	int _laneIndex = 0;
+	float _distance = 0;
+
 
 	public int LaneIndex { get { return _laneIndex; } }
 
 	void Start()
 	{
-		this.transform.DOMove(_lanePositions[_laneIndex].transform.position, 1f);
+		this.transform.DOMove(_laneCameraPositions[_laneIndex].transform.position, 1f);
 	}
 
 	void Update()
 	{
 		this.transform.DOLookAt(_cameraFocus.transform.position, 0);
 		UpdateLaneIndex();
-
+		if (_distance > 0)
+		{
+			
+			var tmp = Vector3.Slerp(new Vector2(_laneCameraPositions[_laneIndex].position.x, _laneCameraPositions[_laneIndex].position.z), new Vector2(_laneCameraPositions[_preLaneIndex].position.x, _laneCameraPositions[_preLaneIndex].position.z), _distance);
+			tmp.z = tmp.y;
+			tmp.y = this.transform.position.y;
+			this.transform.position = tmp;
+			_distance -= Time.deltaTime;
+		}
 	}
 
 	void UpdateLaneIndex()
 	{
 		if (Input.GetKeyDown(KeyCode.A))
 		{
+			_preLaneIndex = _laneIndex;
 			if (_laneIndex > 0)
 			{
 				_laneIndex--;
 			}
 			else
 			{
-				_laneIndex = _lanePositions.Length - 1;
+				_laneIndex = _laneCameraPositions.Length - 1;
 			}
-			this.transform.DOMove(_lanePositions[_laneIndex].transform.position, 2.5f);
+			_distance = 1;
 		}
 		else if (Input.GetKeyDown(KeyCode.D))
 		{
-			if (_laneIndex < _lanePositions.Length - 1)
+			_preLaneIndex = _laneIndex;
+			if (_laneIndex < _laneCameraPositions.Length - 1)
 			{
 				_laneIndex++;
 			}
@@ -46,7 +60,7 @@ public class CameraMovement : MonoBehaviour
 			{
 				_laneIndex = 0;
 			}
-			this.transform.DOMove(_lanePositions[_laneIndex].transform.position, 2.5f);
+			_distance = 1;
 		}
 
 	}
