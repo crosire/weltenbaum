ab4aa92e54067b84c55708f0c971c5a4bef5205f
crosire <crosiredev@gmail.com>
2016-01-30 11:20:42 +0100

    Add waypoint following code to path follow agent script

--- Assets/Game/PathFollowAgent.cs
+++ Assets/Game/PathFollowAgent.cs
@@ -6,17 +6,41 @@ public class PathFollowAgent : MonoBehaviour
 	#region Inspector Variables
 	#endregion
 
+	Entity _entity;
 	NavMeshAgent _agent;
+	bool _isStopped = false;
 
-	public int Lane { get; set; }
-	public int Waypoint { get; private set; }
+	public int LaneIndex { get; set; }
+	public int WaypointIndex { get; private set; }
 
 	void Awake()
 	{
+		_entity = GetComponent<Entity>();
 		_agent = GetComponent<NavMeshAgent>();
 	}
 
 	void Update()
 	{
+		if (_entity._entityState != EntityState.Walk)
+		{
+			if (!_isStopped)
+			{
+				_agent.Stop();
+				_isStopped = true;
+			}
+			return;
+		}
+		else if (_isStopped)
+		{
+			_agent.Resume();
+			_isStopped = false;
+		}
+
+		var waypoint = LaneManager.GetWaypoint(LaneIndex, WaypointIndex);
+
+		if (_agent.remainingDistance < 1.0f)
+		{
+			_agent.destination = LaneManager.GetWaypoint(LaneIndex, ++WaypointIndex);
+		}
 	}
 }
